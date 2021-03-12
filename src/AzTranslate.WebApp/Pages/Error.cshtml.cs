727ad271a100610195a11b798d3af91cbae8e48f
Tariq Abubaker <taq.abubaker@gmail.com>
2021-03-12 22:58:46 +0300

    fixes #5 UI fixes & enhancements
    * make lists focus on last added text
    * remove .WebApp from the title
    * fix home page description
    * fixed "TranslationJobDetails" page title and add about me page

--- src/AzTranslate.WebApp/Pages/Error.cshtml.cs
+++ src/AzTranslate.WebApp/Pages/Error.cshtml.cs
@@ -1,27 +1,20 @@
+using AzTranslate.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AzTranslate.WebApp.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
-    public class ErrorModel : PageModel
+    public class ErrorModel : PageModelBase<ErrorModel>
     {
         public string RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
-        private readonly ILogger<ErrorModel> _logger;
-
-        public ErrorModel(ILogger<ErrorModel> logger)
+        public ErrorModel(IYouTubeServices youTubeServices, ISpeechServices speechServices, ILogger<ErrorModel> logger)
+            : base(youTubeServices, speechServices, logger)
         {
-            _logger = logger;
         }
 
         public void OnGet()
